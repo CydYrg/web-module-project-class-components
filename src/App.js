20d4859aac1c402af79aedf34348e32827589281
Ceyda Yurga <ceydayurga@Ceydas-MacBook-Air.local>
2021-07-05 21:35:11 +0100

    worked on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+class ToDo extends React.Component {
+  render() {
+    return (<div>
+      <h1>Lets do {this.props.name}</h1>
+      <h2>Lets do {this.props.id}</h2>
+    </div>)
+  }
+}
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -7,12 +16,25 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      // test
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
     }
+  }
+
+  handleClick = () => {
+    this.setState({
+      ...this.state,
+      task: 'Do laundry',
+      id: 1528817084358,
+      completed: false
+    });
   }
   render() {
     return (
       <div>
+        <ToDo task={this.state.task} id={this.state.id}/>
+        <button onClick={this.handleClick}>Add Todo</button>
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
