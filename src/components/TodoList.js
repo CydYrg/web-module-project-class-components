0945d6a47df300d146e6d7a5129d7f732011ab55
Ceyda Yurga <ceydayurga@Ceydas-MacBook-Air.local>
2021-07-05 22:02:33 +0100

    worked on ToDoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,25 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import { tsAnyKeyword } from "@babel/types";
+import React from "react";
+
+import Item from "./Item";
+
+const ToDoList = props => {
+  const handleClick = () => {
+      props.clearCompleted();
+  }
+
+   return (
+       <div className="todo-list">
+           {props.todo.map(item => (
+               <Todo toggleTodo={props.toggleTodo} key={task.id} todo={todo} />
+           ))}
+           <button onClick={handleClick} className="clear=btn">
+               Clear Completed
+           </button>
+       </div>
+   );
+};
+
+export default ToDoList;
